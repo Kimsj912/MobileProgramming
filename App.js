2613ad645b6b74704a45840a29f7a184e18b46ed
sujk912 <aa991122aa@gmail.com>
2022-05-06 11:07:46 +0900

    Week9 simple calculator

--- App.js
+++ App.js
@@ -1,136 +1,62 @@
-import * as Location from "expo-location";
-import React, {useEffect,useState} from "react";
-import { ActivityIndicator, View, Text, Dimensions, StyleSheet, ScrollView } from "react-native";
-// Constants
-const API_KEY = "0c64d2a74e1235da0dd38d8e7f441b69";
-const WEEK_DAY = ["일","월","화","수","목","금","토"];
+import React, { useState, useEffect } from 'react';
+import {View, StyleSheet, Text, Button, TextInput } from 'react-native';
 
-// func
-export const getWeatherDay = (data) => {
-    const currentTime = new Date(
-       data * 1000 - 9 * 60 * 60 * 1000
-    );
-    const getMonth = currentTime.getMonth();
-    const getDate = currentTime.getDate();
-    const getWeek = currentTime.getDay();
-    return `${getMonth}월 ${getDate}일 (${WEEK_DAY[getWeek]})`;
-};
-
-export const getWeatherTime = (data) => {
-    const currentTime = new Date(
-       data * 1000 - 9 * 60 * 60 * 1000
-    );
-    const getHour = currentTime.getHours();
-    const getMinute = currentTime.getMinutes();
-    const getSec = currentTime.getSeconds();
-    return `${getHour}시 ${getMinute}분 ${getSec}초`;
-};
-
-export default function App() {
-  // hooks
-    const [days, setDays] = useState([]);
-    const [region, setRegion] = useState("...");
-    const [city, setCity] = useState("Loading");
-    const [district, setDistrict] = useState("...");
-
+const App = () => {
+    const [num, setNum] = useState(1);  
+    const [name, setName] = useState("");
     useEffect(() => {
-    (async () => {
-        const { granted } = await Location.requestForegroundPermissionsAsync();
-        if (!granted) {
-        return;
-        }
-      //경도 위도 정보가져오기
-        const { coords: { latitude, longitude }, } = await Location.getCurrentPositionAsync({ accuracy: 5 });
-        const location = await Location.reverseGeocodeAsync(
-            { latitude, longitude},
-            { useGoogleMaps: false}
-        );
-        setCity(location[0].city);
-        setRegion(location[0].region);
-        setDistrict(location[0].district);
-        const response = await fetch(
-          `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=metric&lang=kr`
-        );
-        const json = await response.json();
-        setDays(json.daily);
-    })();
-    }, []);
+        console.log("mount (최초 1회만)");
+    },[])
+    useEffect(() => {
+        console.log("mount + update");
+    })
+    useEffect(() => {
+        console.log("mount + name이 update될 때");
+    }, [name])
 
     return (
     <View style={styles.container}>
-        <View style={styles.city}>
-            <Text style={styles.notation}>지금 당신이 있는</Text>
-            <Text style={styles.cityName}>{region} {city} {district}</Text>
+        <View style={styles.wrapper}>
+            <Text style={{fontSize:80}}>9th week</Text>
+            <Text style={styles.title}>count : {num}</Text>
+            <Text style={styles.title}>name : {name}</Text>
+            <TextInput style={styles.textInput} onChangeText={e=>setName(e)} />
         </View>
-        <ScrollView
-            horizontal
-            pagingEnabled
-            showsHorizontalScrollIndicator={true}
-            contentContainerStyle={styles.weather}
-        >
-        {/* 0이면 로딩표시 (ActivityIndicator 동작) */}
-        {days.length === 0 ? (
-            <View style={styles.day}>
-                <ActivityIndicator
-                color="white"
-                style={{ marginTop: 10 }}
-                size="large"
-                />
-            </View>
-        ) : (
-            days.map((day, index) => (
-                <View key={index} style={styles.day}>
-                    <Text style={styles.boldbig}>{getWeatherDay(day.dt)} </Text>
-                    <Text style={styles.description}>{day.weather[0].description} <Text style={styles.tinyText}>( {day.weather[0].main} )</Text></Text>
-                    <Text style={styles.boldbig}>현재 기온 : {parseFloat(day.temp.day).toFixed(1)}°C</Text>
-
-                    <View style={styles.detail}>
-                      <Text style={styles.description}>최저기온 : {day.temp.min}°C </Text>
-                      <Text style={styles.description}>최고기온 : {day.temp.max}°C </Text>
-                      <Text style={styles.description}>일출 : {getWeatherTime(day.sunrise)} </Text>
-                      <Text style={styles.description}>일몰 : {getWeatherTime(day.sunset)}</Text>
-                    </View>
-                </View>
-            ))
-            )}
-        </ScrollView>
+        <View style={styles.wrapper}>
+            <Button title='-1' onPress={()=>{setNum(num-1)}} />
+            <Button title='+1' onPress={()=>{setNum(num+1)}} />
+            <Button title='2x' onPress={()=>{setNum(num*2)}} />
+            <Button title='2/' onPress={()=>{setNum(num/2)}} />
+            <Button title='clear' onPress={()=>{setNum(1)}} />
+        </View>
     </View>
     );
-}
-const { width: SCREEN_WIDTH } = Dimensions.get("window");
+};  
+
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: "lightgreen",
+        flex:1 ,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#ffffff',
+        width: `60vw`,
+        alignSelf: 'center',
     },
-    notation: {
-        fontSize: 20,
-        fontWeight: "400",
+    wrapper:{
+        width: `100%`,
+        marginTop: 20,
+        gap: 10,
     },
-    city: {
-        flex: 1.2,
-        justifyContent: "flex-end",
-        alignItems: "center",
+    textInput:{
+        width: `100%`,
+        height: `2rem`,
+        fontSize: '2rem',
+        borderBottomColor: 'gray',
+        borderBottomWidth: 2,
     },
-    cityName: {
+    title: {
         fontSize: 30,
-        fontWeight: "500",
-    },
-    boldbig: {
-      marginTop: 20,
-      fontSize: 40,
-      fontWeight: "600",
-      textAlign: "center",
-    },
-    day: {
-      width: SCREEN_WIDTH,
-      justifyContent: "flex-start",
-      alignItems: "center",
-    },
-    detail:{
-      margin: 15,
-    },
-    description: {
-      fontSize: 30,
     },
 });
+
+export default App;
