8a526a6bcd52d0c3d39af3326b2675578046f1fb
sujk912 <aa991122aa@gmail.com>
2022-05-27 11:34:48 +0900

    Week 12 - navigation Feature

--- App.js
+++ App.js
@@ -1,15 +1,3 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import StackNavigation from './navigations/Stack';
-import TabNavigation from './navigations/Tab';
-
-const App = () => {
-  return (
-    <NavigationContainer>
-      {/* <StackNavigation /> */}
-      <TabNavigation />
-    </NavigationContainer>
-  );
-};
+import App from './src/App';
 
 export default App;
